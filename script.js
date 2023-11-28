73aadb9d8e149f516e7b4e12a9710334bdc0b121
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-28 11:08:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+	square.addEventListener('mouseover', () => {
+		const current = square;
+		squares.forEach(othersquare => {
+			if(current != othersquare){
+				othersquare.style.backgroundColor = '#6F4E37';
+			}  
+		});
+	});
+
+	square.addEventListener('mouseout', () => {
+		squares.forEach(allsquare => {
+			allsquare.style.backgroundColor = '#E6E6FA';
+		});
+	});
+});
